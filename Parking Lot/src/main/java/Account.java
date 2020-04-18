b8db16be0af00bd25bde83330340818b121d5bd4
Arpit <arpit.agarwal181@gmail.com>
2020-04-18 23:36:04 +0530

    add getter setter

--- Parking Lot/src/main/java/Account.java
+++ Parking Lot/src/main/java/Account.java
@@ -3,6 +3,39 @@ public abstract class Account {
     private String password;
     private AccountStatus status;
     private Person person;
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public AccountStatus getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(AccountStatus status) {
+		this.status = status;
+	}
+
+	public Person getPerson() {
+		return this.person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
   
     public boolean resetPassword();
   }
--- Parking Lot/src/main/java/Address.java
+++ Parking Lot/src/main/java/Address.java
@@ -4,5 +4,46 @@ public class Address {
     private String state;
     private String zipCode;
     private String country;
+
+	public String getStreetAddress() {
+		return this.streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return this.zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
   }
   
