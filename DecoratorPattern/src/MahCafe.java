0ddf665e2c047b1ab41e5bd12cf3701b2ade1b09
Arpit <arpit.agarwal181@gmail.com>
2020-04-28 00:04:16 +0530

    add Decorator Pattern

---
+++ DecoratorPattern/src/MahCafe.java
@@ -0,0 +1,17 @@
+public class MahCafe {
+    public static void main(String[] args) {
+        Beverage beverage1 = new Espresso();
+        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
+
+        Beverage beverage2 = new HouseBlend();
+        beverage2 = new Mocha(beverage2);
+        beverage2 = new Mocha(beverage2);
+        beverage2 = new Soy(beverage2);
+        beverage2 = new Whip(beverage2);
+
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+
+        beverage1 = new Whip(beverage1);
+        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
+    }
+}
---
+++ DecoratorPattern/src/Soy.java
@@ -0,0 +1,17 @@
+public class Soy extends ExtrasDecorator {
+    Beverage beverage;
+
+    public Soy(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Soy";
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + 0.15;
+    }
+}
